f2a66b5e741ffbd65986f908694c73a41b56258d
Murilo Dutra <murilodut@gmail.com>
2017-06-03 10:15:36 -0300

    Add example 2

---
+++ exemplo2.ts
@@ -0,0 +1,9 @@
+# Exemplo 2
+real y é 123,8, z é 131,123. int x. leia x. texto mensagem é "Olá, mundo!".
+
+se x é menor ou igual a 10 então decrementa x. e deu.
+mostra mensagem. mostra soma x, y.
+
+# leia arquivo daqui.
+
+define soma com int x, real y como retorna x mais y. e deu.
